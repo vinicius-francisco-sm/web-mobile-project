225afe395f0622832191a7626a3d65de8c822e21
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2024-04-08 17:49:09 -0300

    feat: nav layout

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -8,23 +8,25 @@ export function MyNav(){
         <>
             <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <Nav.Link href="#link">Link-1</Nav.Link>
+                    <Nav.Link href="#home">Link-2</Nav.Link>
+                    <Nav.Link href="#link">Link-3</Nav.Link>
+                    <Nav.Link href="#home">Link-4</Nav.Link>
+                    <Nav.Link href="#link">Link-5</Nav.Link>
+                    {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                        Another action
-                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">
                         Separated link
                     </NavDropdown.Item>
-                    </NavDropdown>
+                    </NavDropdown> */}
                 </Nav>
                 </Navbar.Collapse>
             </Container>
