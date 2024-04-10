8be3b7b4f319fdd001a93b6e285f7ababeb1fcc0
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2024-04-10 20:41:41 -0300

    feat: styling footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,14 @@ export function Footer(){
     return(
         <>
             <footer className="bg-light">
-                <h1>Footer</h1>
+                <div className="footer-content">
+                    <ul>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">Discord</a></li>
+                        <li><a href="">Linkedin</a></li>
+                        <li><a href="">Github</a></li>
+                    </ul>
+                </div>
             </footer>
         </>
     )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/main.min.css';
 import '@/components/Header/Header.css'
 import '@/components/Layout/layout.css'
 import { Ysabeau_Infant } from "next/font/google";
+import '@/components/Footer/footer.css'
 
 const infant = Ysabeau_Infant({
   subsets: ['latin'],
