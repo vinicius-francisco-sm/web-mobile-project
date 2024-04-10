4b0bb2a83377ae87f5e5cf4a251930fda118a5a5
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2024-04-10 18:20:26 -0300

    feat: filling login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,17 @@
 import { Layout } from "@/components/Layout/Layout";
+import { Button, Card } from "react-bootstrap";
 
 export default function Login(){
     return(
         <Layout>
-            <h1>Login</h1>
+            <Card className="p-5 m-5 mw-50">
+                <form action="" className="container text-center d-flex flex-column">
+                    <h1>Faça o Login</h1><hr />
+                    <input className="my-2" type="email" placeholder="Digite seu e-mail" />
+                    <input className="my-2" type="password" placeholder="Digite sua senha" />
+                    <Button>Entrar</Button>
+                </form>
+            </Card>
         </Layout>
     )
 }
